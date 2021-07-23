0988d9548185e14c1f49a80a62fe87e3c0f552c5
Micah Steinbrecher <micahsteinbrecher@wustl.edu>
2021-07-23 11:08:59 -0500

    server backup

--- server.js
+++ server.js
@@ -39,38 +39,15 @@ app.get('/searchcontacts', async (req, res) => {
 	  
 	})
 
+
+
 	
-	input = req.query.input
-	console.log("input: " + input)
+	input = req.query.input.split(",")
+	console.log("input: ")
+	console.log(input)
 
 	let ids = []
-	let contacts = []
-	if (typeof input === 'string') {
-		input = input.toLowerCase();
-		ids = await sql`
-			select distinct contact_id 
-			from address where
-			city = ${input} or
-			state = ${input}
-		`		
-		console.log('found ids:')
-
-		console.log(ids)
-
-	
-		for (var i = 0; i < ids.length; i++) {
-		    result = await sql`
-				select * 
-				from contact where
-				contact_id = ${ids[i].contact_id}
-			`
-			contacts.push(result[0])
-
-
-		}
-		console.log('found contacts:')
-
-	}
+	let contacts = await contactSearch(input)
 
 	console.log(contacts)
 
@@ -78,6 +55,97 @@ app.get('/searchcontacts', async (req, res) => {
 
 
 })
+
+async function contactSearch(inputs) {
+	const sql = postgres({
+	  database	  : 'contact_list',
+	  host        : 'localhost',         // Postgres ip address or domain name
+	  port        : 5432,       // Postgres server port
+	  username    : 'contacts',         // Username of database user
+	  password    : 'contacts',         // Password of database user
+	  
+	})
+
+	ids = []
+	contacts = []
+
+	for (var i = 0; i < inputs.length; i++) {
+
+		if (typeof inputs[i] === 'string') {
+			console.log('string search')
+
+			input = inputs[i].toLowerCase();
+			//get contact ids for matching addresses
+			let result = await sql`
+				select distinct contact_id 
+				from contact
+				where
+				fname = ${input} or
+				mname = ${input} or
+				lname = ${input} or
+				
+			`
+
+			ids.push(...result)
+
+			let result = await sql`
+				select distinct contact_id 
+				from address
+				where
+				city = ${input} or
+				state = ${input} 
+			`
+
+			address = ${input} or
+				city = ${input} or
+				state = ${input} 
+			console.log('search by string results ')
+			console.log(results)
+
+			console.log('got ids by string: ')
+			console.log(ids)
+
+
+		}
+
+		if (typeof input[i] === 'number') {
+
+			input = input.toLowerCase();
+			//get contact ids for matching addresses
+			let result = await sql`
+				select distinct contact_id 
+				address as a,
+				phone as p
+				where
+				zip = ${input} or
+				area_code = ${input} or
+				number = ${input} or
+			`
+
+			ids.push(...result)
+		}
+	}
+
+	ids = [...new Set(ids)];
+
+
+	for (var i = 0; i < ids.length; i++) {
+	    result = await sql`
+			select * 
+			from contact where
+			contact_id = ${ids[i].contact_id}
+		`
+		contacts.push(result[0])
+
+
+	}
+
+	return contacts
+	
+
+}
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
